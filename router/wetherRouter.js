58e6d45fbc525281d6bf1a1104a1ecf2975675cf
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-04-11 17:52:26 +0530

    update

--- router/wetherRouter.js
+++ router/wetherRouter.js
@@ -1,8 +1,14 @@
 const express=require('express');
 const weatherRouter=express.Router();
 
-weatherRouter.post('/weather',async(req,res,next)=>{
+weatherRouter.post('/',async(req,res,next)=>{
     const city=req.body.city;
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'0bfd319edcbd0ebbfe80278441cd160a'}&units=metric`);
+    const data=await response.json();
+    res.render('index',{mosum:data.weather[0].description,city:data.name,main:data.main,wind:data.wind.speed});
+});
+weatherRouter.get('/',async(req,res,next)=>{
+    let city='Patna';
     const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'0bfd319edcbd0ebbfe80278441cd160a'}&units=metric`);
     const data=await response.json();
     res.render('index',{mosum:data.weather[0].description,city:data.name,main:data.main,wind:data.wind.speed});
