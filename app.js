6c3af05131b39c2b3842e81a87613277c211aa7a
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-04-12 22:09:16 +0530

    new features

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app=express();
 app.set('view engine','ejs');
 app.set('views','views');
 app.use(express.urlencoded());
+app.use(express.json());
 app.use(weatherRouter);
 app.listen(3001,()=>{
     console.log('server start on http://localhost:3001');
--- router/wetherRouter.js
+++ router/wetherRouter.js
@@ -1,17 +1,27 @@
 const express=require('express');
 const weatherRouter=express.Router();
 
-weatherRouter.post('/',async(req,res,next)=>{
-    const city=req.body.city;
-    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'0bfd319edcbd0ebbfe80278441cd160a'}&units=metric`);
+weatherRouter.get('/loc',async(req,res,next)=>{
+    const {lat,lon}=req.query;
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=31.5766068&lon=74.9912212&appid=0bfd319edcbd0ebbfe80278441cd160a`);
     const data=await response.json();
     const sambhav=parseFloat((3.6*parseFloat(data.wind.speed)).toFixed(2));
-    console.log(sambhav);
-    res.render('index',{mosum:data.weather[0].description,city:data.name,main:data.main,wind:sambhav});
+    res.render('index copy',{mosum:data.weather[0].description,city:data.name,main:data.main,wind:sambhav});
 });
+
+
+weatherRouter.post('/',async(req,res,next)=>{
+    const city=req.body.city;
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'0bfd319edcbd0ebbfe80278441cd160a'}`);
+    const data=await response.json();
+    const sambhav=parseFloat((3.6*parseFloat(data.wind.speed)).toFixed(2));
+    res.render('index copy',{mosum:data.weather[0].description,city:data.name,main:data.main,wind:sambhav});
+});
+
+
 weatherRouter.get('/',async(req,res,next)=>{
     let city='Patna';
-    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'0bfd319edcbd0ebbfe80278441cd160a'}&units=metric`);
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'0bfd319edcbd0ebbfe80278441cd160a'}`);
     const data=await response.json();
     res.render('index',{mosum:data.weather[0].description,city:data.name,main:data.main,wind:data.wind.speed});
 });
